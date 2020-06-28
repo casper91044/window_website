f669fa1e837022be7591edcc764a6644e8ab7e81
casper91044 <casper91044@mail.ru>
2020-06-28 22:24:44 +0300

    Part#3,5

--- src/js/main.js
+++ src/js/main.js
@@ -6,10 +6,15 @@ import forms from "./modules/forms";
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
+
+    let modalState = {};
+
     modals();
     tabs('.glazing_slider', '.glazing_block',
         '.glazing_content', 'active');
     tabs('.decoration_slider', '.no_click',
         '.decoration_content > div > div', 'after_click');
+    tabs('.balcon_icons', '.balcon_icons_img',
+        '.big_img>img', 'do_image_more', 'inline-block');
     forms();
 });
